22fbaeb18aebeb8ff83c6b52067954d3b3ddddf8
Carlos Vieira <edu.carlos.vieira@gmail.com>
2018-04-18 09:27:37 -0300

    Refatora dockerfile e corrigi template

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM python:2-alpine3.7
 
 COPY requirements.txt .
+
+RUN apk --update add ttf-dejavu fontconfig openjdk8-jre && \
+    pip install -r requirements.txt && \
+    mkdir -p output
+
 COPY pdf_writer.py .
 COPY templates/ templates/
-
-RUN apk --update add ttf-dejavu fontconfig openjdk8-jre && pip install -r requirements.txt && mkdir -p output
 
 ENTRYPOINT ["python2", "pdf_writer.py"]
 
--- pdf_writer.py
+++ pdf_writer.py
@@ -32,9 +32,9 @@ def filter_parameters(request_args):
 
 @app.route('/')
 def my_route():
-  
+
   parameters = filter_parameters(request.args.to_dict())
-  
+
   sign = hmac.new(secret, json.dumps(parameters), hashlib.sha512).hexdigest()
   parameters['hash_value'] = sign
   processing(parameters)
@@ -48,16 +48,14 @@ def my_route():
       return resposta
   except IOError:
       return make_response("<h1>403 Forbidden</h1>", 403)
-  
+
 @app.route("/validar/<hash_value>")
 def check_hash(hash_value):
-    if hash_value == "41b6b90efc34268fbaae7e142b9ee5412c996d901ec117f090":
+    if hash_value == "41b6b90efc34268fbaae7e142b9ee5412c996d901ec117f090bc3950138ffff77d0bc42e4d615d122a2f04a7a4ac68d9e2e7af7553aaae7dd43d088fa6d08cd0":
       return make_response("<h1>Hash correto</h1>", 200)
     else:
-      return make_response("<h1> Inválido</h1>", 403)
+      return make_response("<h1>403 Inválido</h1>", 403)
 
 if __name__ == '__main__':
     compiling()
     app.run(host='0.0.0.0')
-
-
